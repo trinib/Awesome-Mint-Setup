73a80ae3170a0e2999d78e181e00cd804935fa08
☣┌͜∩͜┐͜(͜◣͜_͜◢͜)͜┌͜∩͜┐☣ <watchtv4ever@gmail.com>
2026-05-09 03:14:29 -0400

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -55,7 +55,7 @@ config.enable_scroll_bar = false
 -- Appearance settings
 config.font = wezterm.font_with_fallback({
   {
-    family = 'FiraCode Nerd Font Ret',
+    family = 'FiraCode Nerd Font Med',
     weight = 'Regular',
     harfbuzz_features = {'calt=1', 'clig=1', 'liga=1'},
   },
