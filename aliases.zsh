5f620e1aa03ba9b8d73e7c61dd17b7f6096e9ecf
☣┌͜∩͜┐͜(͜◣͜_͜◢͜)͜┌͜∩͜┐☣ <watchtv4ever@gmail.com>
2026-07-17 15:37:05 -0400

    Add files via upload

---
+++ aliases.zsh
@@ -0,0 +1,33 @@
+# System commands
+alias e="zsh"
+alias c="clear"
+alias ed="$EDITOR"
+alias o="xdg-open"
+alias g="git"
+alias k="kubectl"
+alias dk="docker"
+alias dc="docker-compose"
+
+# Directory navigation
+alias h="cd ~"
+alias dl="cd ~/Downloads"
+alias doc="cd ~/Documents"
+alias dev="cd ~/Development"
+
+# Package management (adapt to your system)
+alias ai="sudo apt install"
+alias au="sudo apt update"
+alias aug="sudo apt upgrade"
+
+# Git abbreviations
+alias gaa="git add --all"
+alias gcm="git commit -m"
+alias gco="git checkout"
+alias gpl="git pull"
+alias gps="git push"
+alias gst="git status"
+alias gbr="git branch"
+
+# Network
+alias myip="curl ifconfig.me"
+alias ports="sudo netstat -tulanp"
